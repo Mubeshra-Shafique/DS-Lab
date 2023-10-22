6de0d53add6c61e4f5b35127a81b79faecb963af
Mubeshra Shafique <147998735+Mubeshra-Shafique@users.noreply.github.com>
2023-10-22 21:17:28 +0400

    Add files via upload

--- labs task/circularall.cpp
+++ labs task/circularall.cpp
@@ -71,7 +71,67 @@ public:
         p->next = ptr->next;
         ptr->next = p;
     }
-
+void del_at_beg(){
+	if(head==NULL){
+		cout<<"empty";
+	}
+	else if(head->next==head){
+		delete head;
+		head=NULL;
+	}
+	else{	
+	Node *ptr,*p;
+	ptr=head;
+	while(ptr->next!=head){
+		ptr=ptr->next;
+	}
+	p=head;
+	head=head->next;
+	ptr->next=head;
+	delete p;
+	p=NULL;
+}
+}
+void del_at_end(){
+	if(head==NULL){
+		cout<<"empty";
+	}
+	else if(head->next==head){
+		delete head;
+		head=NULL;
+	}
+	else{	
+	Node *ptr,*p;
+	ptr=head;
+	while(ptr->next!=head){
+		p=ptr;
+		ptr=ptr->next;
+	}
+	p->next=head;
+	delete ptr;
+	ptr=NULL;
+}
+}
+void del_at_spec(int pos){
+	if(head==NULL){
+		cout<<"empty";
+	}
+	else if(head->next==head){
+		delete head;
+		head=NULL;
+	}
+	else{	
+	Node *ptr,*p;
+	ptr=head;
+	while(ptr->data!=pos){
+		p=ptr;
+		ptr=ptr->next;
+	}
+	p->next=ptr->next;
+	delete ptr;
+	ptr=NULL;
+}
+}
     void display() {
         if (head == nullptr) {
             cout << "List is empty." << endl;
@@ -85,57 +145,70 @@ public:
         cout << endl;
     }
 };
-
-int main() {
-    CircularLinkedList cll;
-    int ch;
+int main(){
+	CircularLinkedList n;
+	int ch;
 	do{
 		cout<<endl;
 		cout<<"Press 1 for insert a nodes"<<endl;
-		cout<<"Press 2 for insert node at end "<<endl;
-		cout<<"Press 3 for insert at specific value "<<endl;
-		cout<<"Press 4 for display"<<endl;
-		cout<<"Press 5 for exit"<<endl;
+		cout<<"Press 2 for insert at specific value "<<endl;
+		cout<<"Press 3 for display"<<endl;
+		cout<<"Press 4 for deletion at begining"<<endl;
+		cout<<"Press 5 for deletion at end"<<endl;
+		cout<<"Press 6 for deletion at specific value"<<endl;
+		cout<<"Press 7 for exit"<<endl;
 		cout<<"Enter your choice:"<<endl;
 		cin>>ch;
 		
 	if(ch==1){
 
 			cout<<"insert a nodes"<<endl;
-				
-               cll.insert(1);
-               cll.insert(2);
-               cll.insert(3);
-               cll.insert(4);
-
-				 	}
+				n.insert(1);
+				n.insert(2);
+	            n.insert(3);
+             	n.insert(4);
+            	}
 		
-		
-   else if(ch==2){
-   
-	cout<<"insert a nodes at end"<<endl;
-				cll.insert_end(5);
-			}
-	
-    else if(ch==3){
+    else if(ch==2){
 	
 	cout<<"insert at specific value "<<endl;	
-				cll.insert_at_value(3, 7);
+				n.insert_at_value(3,7);
 				}
-    else if(ch==4){
+    else if(ch==3){
 	
 	cout<<"For display "<<endl;	
 			
-				cll.display();
-
-			
+				n.display();	
 			}
+	
+	else if(ch==4){
+	
+	cout<<"Press 4 for deletion at begining"<<endl;
+			
+				n.del_at_beg();
+			}		
+			
+	else if(ch==5){
+	
+	cout<<"Press 5 for deletion at end"<<endl;	
+			
+				n.del_at_end();	
+			}		
+	else if(ch==6){
+	
+	cout<<"Press 6 for deletion at specific value"<<endl;	
+			
+				n.del_at_spec(2);
+			}			
+			
    else{
    	cout<<"invalid choice";
    }
 	
 	}
-while(ch!=5);
+while(ch!=7);
+
 	
 	return 0;
 }
+
